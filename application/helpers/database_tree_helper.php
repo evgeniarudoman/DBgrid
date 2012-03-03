e828238bda8b67e2136ad48e57ae20ef105ed569
Evgenia Rudoman <evgenia.rudoman@gmail.com>
2012-03-03 17:15:06 +0200

    added new logic

---
+++ application/helpers/database_tree_helper.php
@@ -0,0 +1,73 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+if (!function_exists('get_database_tree'))
+{
+
+    function get_database_tree($user_id)
+    {
+        $grid = &get_instance();
+
+        $grid->load->model('database');
+        $grid->load->model('table');
+        $grid->load->model('field');
+        $grid->load->model('type');
+
+        $databases = $grid->database->load_collection("dbgrid", array('user_id' => $user_id));
+
+        foreach ($databases as $database)
+        {
+            $result['databases'][$database->getId()] = $database->getName();
+
+            $tables = $grid->table->load_collection("dbgrid", array('user_id' => $user_id, 'db_id' => $database->getId()));
+
+            foreach ($tables as $table)
+            {
+                $result[$database->getName() . '_table'][$table->getId()] = $table->getName();
+
+                $fields = $grid->field->load_collection("dbgrid", array('user_id' => $user_id, 'table_id' => $table->getId()));
+
+                foreach ($fields as $field)
+                {
+
+                    $type_id = $field->getTypeId();
+                    $grid->type->db_name = "dbgrid";
+
+                    $result[$database->getName() . '_field'][$field->getId()]['name'] = $field->getName();
+
+                    $grid->type->select(array('id' => $type_id));
+                    $result[$database->getName() . '_field'][$field->getId()]['type'] = $grid->type->getType();
+                }
+            }
+        }
+
+        return $result;
+    }
+
+}
+
+
+if (!function_exists('db_table_exists'))
+{
+
+    function db_table_exists($user_id, $db_name, $table_name)
+    {
+        $grid = &get_instance();
+
+        $grid->load->model('database');
+        $grid->load->model('table');
+
+        $databases = $grid->database->load_collection("dbgrid", array('user_id' => $user_id, 'name' => $db_name));
+        $tables = $grid->table->load_collection("dbgrid", array('user_id' => $user_id, 'name' => $table_name));
+        
+        if (isset($databases) && empty($databases) || isset($tables) && empty($tables))
+            $err = 1;
+        else 
+            $err = 0;
+        
+        return $err;
+    }
+
+}
